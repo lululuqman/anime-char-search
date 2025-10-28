58c5ee9b7c8ced841540bdb2f0355a01bb83d8cc
Luqman Hakim <ayambaik77@gmail.com>
2025-10-28 08:54:04 +0800

    change tab text

--- Frontend/src/hooks/useFavorites.js
+++ Frontend/src/hooks/useFavorites.js
@@ -1,31 +1,31 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-export default function useFavorites() {
-  const [favorites, setFavorites] = useState([]);
+// export default function useFavorites() {
+//   const [favorites, setFavorites] = useState([]);
 
-  // Load favorites from localStorage when app loads
-  useEffect(() => {
-    const saved = localStorage.getItem("favorites");
-    if (saved) setFavorites(JSON.parse(saved));
-  }, []);
+//   // Load favorites from localStorage when app loads
+//   useEffect(() => {
+//     const saved = localStorage.getItem("favorites");
+//     if (saved) setFavorites(JSON.parse(saved));
+//   }, []);
 
-  // Save favorites to localStorage whenever they change
-  useEffect(() => {
-    localStorage.setItem("favorites", JSON.stringify(favorites));
-  }, [favorites]);
+//   // Save favorites to localStorage whenever they change
+//   useEffect(() => {
+//     localStorage.setItem("favorites", JSON.stringify(favorites));
+//   }, [favorites]);
 
-  const toggleFavorite = (char) => {
-    setFavorites((prev) => {
-      const exists = prev.find((f) => f.mal_id === char.mal_id);
-      if (exists) {
-        return prev.filter((f) => f.mal_id !== char.mal_id);
-      } else {
-        return [...prev, char];
-      }
-    });
-  };
+//   const toggleFavorite = (char) => {
+//     setFavorites((prev) => {
+//       const exists = prev.find((f) => f.mal_id === char.mal_id);
+//       if (exists) {
+//         return prev.filter((f) => f.mal_id !== char.mal_id);
+//       } else {
+//         return [...prev, char];
+//       }
+//     });
+//   };
 
-  const isFavorite = (id) => favorites.some((f) => f.mal_id === id);
+//   const isFavorite = (id) => favorites.some((f) => f.mal_id === id);
 
-  return { favorites, toggleFavorite, isFavorite };
-}
+//   return { favorites, toggleFavorite, isFavorite };
+// }
